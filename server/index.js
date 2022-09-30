54e402cecb1d4b24c7fb8c22f80a6e1005c75f92
Utsav bhattarai <95236817+utsavbhattarai007@users.noreply.github.com>
2022-09-30 16:38:15 +0545

    Db configured

--- server/index.js
+++ server/index.js
@@ -1,13 +1,18 @@
 //Importing dependencies
 import express from "express";
 import {} from "dotenv/config";
+import colors from "colors";
+import {connectDB} from "./conn.js";
 
 //Making Instances
 const app = express();
 const port = 3000;
 
+//Connecting to Database
+connectDB();
+
 //Routes
 app.get('/', (req, res) => res.send('Hello World!'));
 
 //Listening on port
-app.listen(port, () => console.log(`Listening on port ${port}!`));
+app.listen(port, () => console.log(`Listening on port ${port}!`.brightWhite));
